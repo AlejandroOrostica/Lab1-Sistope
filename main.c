e294659ccfd6adc744611c998303616cc4c7f8ff
Claudio <claudio.urrutia.b@usach.cl>
2019-05-07 21:07:53 -0400

    PROKAKO

--- main.c
+++ main.c
@@ -1,6 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char** procesarLinea(char* linea,char** lista){
+
+    int tamano = strlen(linea);
+	char deli[2] = ",";
+
+	char *token = strtok(linea, deli);
+    int j =0;
+	while(token != NULL)
+	{
+		
+		lista[j]=token;
+        token = strtok(NULL, deli);
+        
+        j++;
+	}
+
+
+	
+    return lista;
+	
+} 
 
 int main(){
-    printf("Hola Mundo \n");
+    FILE* archivo;
+    char buffer[100];
+    char* linea= (char*)malloc(sizeof(char)*100);
+    char** lista= (char**)malloc(sizeof(char*)*5);
+    for(int i=0;i<5;i++){
+        lista[i]= (char*)malloc(sizeof(char)*100);
+    }    
+    archivo = fopen("prueba.csv", "r");
+
+    while(!feof(archivo)){
+    int i=0;
+    int pos=0;
+    while (i<1){
+    fread(buffer,1,1, archivo);
+    
+    
+    if(strcmp(buffer,"\n")==0||feof(archivo)){
+        
+        i++;
+    }
+    else{
+        linea[pos]=buffer[0];
+        pos++;
+    }
+    
+    }
+    //procesado de linea
+    lista=procesarLinea(linea,lista);
+        for(int k=0;k<5;k++){
+        printf("%s ", lista[k]);
+        
+
+    }
+   printf("\n");
+    }
+    
     return 0;
 }
