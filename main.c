e48dea73d187619c380228232ab50139d10e00a3
Alejandro <alejandro.orostica@usach.cl>
2019-05-08 21:02:34 -0400

    avance en pipes

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <math.h>
 
 #define LECTURA 0
 #define ESCRITURA 1
@@ -29,18 +30,24 @@ char** procesarLinea(char* linea,char** lista){
 	
     return lista;
 	
-} 
+}
+
+float calcularDistancia(char** lista){
+    float distancia =  sqrtf( powf(atof(lista[0]),2.0) +  powf(atof(lista[1]),2.0) );
+    return distancia;
+}
 
 
 
 int main(int argc, char const *argv[]){
     int pid, radio, numeroDiscos, ppid;
-    numeroDiscos = 2;
+    float distancia;
+    numeroDiscos = 1;
     int* pids = (int*)malloc(sizeof(int)*numeroDiscos);
 
-    int** arregloPipes = (int**)malloc(sizeof(int*)*numeroDiscos*2)
+    int** arregloPipes = (int**)malloc(sizeof(int*)*numeroDiscos*2);
     for(int i=0 ; i<numeroDiscos*2; i++){
-        arregloPipes[i] = (int*)malloc(sizeof(int)*2)
+        arregloPipes[i] = (int*)malloc(sizeof(int)*2);
     }
     
     FILE* archivo;
@@ -48,7 +55,7 @@ int main(int argc, char const *argv[]){
     char* linea= (char*)malloc(sizeof(char)*100);
     char* puntoV = (char*)malloc(sizeof(char)*100);
     char* puntoU = (char*)malloc(sizeof(char)*100);
-    puntoU, puntoV = '\0'
+    puntoU, puntoV = '\0';
     char** lista= (char**)malloc(sizeof(char*)*5);
     for(int i=0;i<5;i++){
         lista[i]= (char*)malloc(sizeof(char)*100);
@@ -59,38 +66,60 @@ int main(int argc, char const *argv[]){
 
     for(int i=0; i<numeroDiscos; i++){
         if(ppid == getpid()){
-            pid = fork()
-            
+            pid = fork();
+            if(pid == 0){
+                pids[i] = getpid();
+            }
         }
     }
 
+    if(ppid == getpid()){
+        archivo = fopen("prueba.csv", "r");
 
-
-    archivo = fopen("prueba.csv", "r");
-
-    while(!feof(archivo)){
-    int i=0;
-    int pos=0;
-        while (i<1){
-            fread(buffer,1,1, archivo);
+        while(!feof(archivo)){
+            int i=0;
+            int pos=0;
+            while (i<1){
+                fread(buffer,1,1, archivo);
     
     
-            if(strcmp(buffer,"\n")==0||feof(archivo)){
+                if(strcmp(buffer,"\n")==0||feof(archivo)){
         
-                i++;
-            }
-            else{
+                    i++;
+                }
+                else{
                 linea[pos]=buffer[0];
                 pos++;
-            }
+                }
     
-        }
-        //procesado de linea
-        lista=procesarLinea(linea,lista);
+            }
+            //procesado de linea
+            lista=procesarLinea(linea,lista);
+            distancia = calcularDistancia(lista);
+
     
     
 
+        }
+        printf("Soy el papi \n");
+        int* pipes =(int*)malloc(sizeof(int)*2);
+        pipe(pipes);
+        close(pipes[LECTURA]);
+        char* string2 = (char *)malloc(10*sizeof(char));
+        string2[0] = '\0';
+        strcpy(string2, "hola");
+        write(pipes[ESCRITURA], string2, sizeof(char)*10);
+        printf("Según yo mandé: %s", string2);
+        fclose(archivo);
     }
+    else{
+        //execve("vis.c");
+        printf("Soy un hijito  \n");
+    }
+
+
+
+    
     
     return 0;
 }
